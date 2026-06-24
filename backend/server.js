43793ed4effdc4b34a0cce5ba6f7c2600f1c3234
LNHP77 <156014853+LNHP77@users.noreply.github.com>
2026-06-24 18:27:45 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,10 @@ const cors = require('cors');
 const app = express();
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
 });
+
 
 app.use(cors({ origin: process.env.FRONTEND_URL || '*' }));
 app.use(express.json());
